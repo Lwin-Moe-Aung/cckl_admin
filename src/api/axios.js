65e689c7a118f9528a73382d1d9448b51dcf2cd9
Lwin-Moe-Aung <lwinmoeaung.ycc@gmail.com>
2023-02-14 02:18:05 +0630

    change base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://54.169.192.233:8080/api/';
+const BASE_URL = 'https://admin.ccklos.org/api/';
 // const BASE_URL = 'http://localhost:8080/api/';
 
 export default axios.create({
